01b5ab3d9c9dabd3f40c0c474993b7fb597bb709
Tom Coemans <tom.coemans@gmail.com>
2019-05-10 21:25:12 +0200

    nieuwe dependencies

--- vl-icon.src.js
+++ vl-icon.src.js
@@ -1,23 +1,24 @@
-import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
+import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
 
 /**
  * VlIcon
  * @class
  * @classdesc Gebruik de vl-icon om een extra visueel element toe te voegen. <a href="../demo/vl-icon.html">Demo</a>.
  * 
- * @extends VlElement
+ * @extends NativeVlElement
  * 
- * @property {string} icoon - Attribuut wordt gebruikt om aan te geven welk icoon getoond moet worden.
+ * @property {string} icon - Attribuut wordt gebruikt om aan te geven welk icoon getoond moet worden.
  * @property {(small | large)} size - Attribuut wordt gebruikt om het icoon te verkleinen (80%) of te vergroten (120%) ten opzichte van de parent.
  * @property {boolean} light - Attribuut wordt gebruikt om het icoon een lichte kleur te geven.
  * @property {boolean} before - Attribuut wordt gebruikt wanneer het icoon voor een tekst staat en er wat ruimte tussen het icoon en de tekst getoond moet worden.
  * @property {boolean} after - Attribuut wordt gebruikt wanneer het icoon achter een tekst staat en er wat ruimte tussen het icoon en de tekst getoond moet worden.
  * @property {boolean} 90deg - Attribuut wordt gebruikt om het icoon 90 graden te roteren.
  * @property {boolean} 180deg - Attribuut wordt gebruikt om het icoon 180 graden te roteren.
+ * @property {boolean} link - Attribuut moet gebruikt worden wanneer het icoon binnen een a tag gebruikt wordt zodat de stijl goed is.
  */
-export class VlIcon extends VlElement(HTMLElement) {
+export class VlIcon extends NativeVlElement(HTMLSpanElement) {
     static get _observedAttributes() {
-        return ['icon', 'size', '90deg', '180deg'];
+        return ['icon', 'size', '90deg', '180deg', 'link'];
     }
 
     static get _observedChildClassAttributes() {
@@ -25,20 +26,18 @@ export class VlIcon extends VlElement(HTMLElement) {
     }
 
     constructor() {
-        super(`
-            <style>
-                @import "../style.css";
-
-                .vl-icon:before {
-                    vertical-align: middle;
-                }
-            </style>
-            <span class="vl-icon vl-vi" aria-hidden="true"></span>
-        `);
+        super();
+        this.classList.add('vl-icon');
+        this.classList.add('vl-vi');
+        this.setAttribute('aria-hidden', true);
     }
 
     get _classPrefix() {
         return 'vl-icon--';
+    }
+
+    get _stylePath() {
+        return '../style.css';
     }
 
     _iconChangedCallback(oldValue, newValue) {
@@ -60,6 +59,18 @@ export class VlIcon extends VlElement(HTMLElement) {
     _180degChangedCallback(oldValue, newValue) {
         this._toggleClass(this._element, newValue, 'vl-vi-u-180deg');
     }
+
+    _linkChangedCallback(oldValue, newValue) {
+        if (newValue != undefined) {
+            this._element.classList.forEach((value) => {
+                this._element.classList.replace(value, value.replace('-icon', '-link__icon'));
+            });
+        } else {
+            this._element.classList.forEach((value) => {
+                this._element.classList.replace(value, value.replace('-link__icon', '-icon'));
+            });
+        }
+    }
 }
 
-customElements.define('vl-icon', VlIcon);
+customElements.define('vl-icon', VlIcon, {extends: 'span'});

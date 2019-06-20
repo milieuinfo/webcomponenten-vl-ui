59ae506c91ac6c4d08bb870e99bc96c0d975fb6a
sbylemans <sander.bylemans@continuum.be>
2019-06-20 15:58:54 +0200

    UIG-21 DSI2-301 vl-ui-modal

---
+++ vl-action-group.src.js
@@ -0,0 +1,40 @@
+import { NativeVlElement } from '/node_modules/vl-ui-core/vl-core.js';
+
+
+/**
+ * VLActionGroup
+ * @class
+ * @classdesc Toon meerdere knoppen of links. De groep zorgt ervoor dat ze correct zijn uitgelijnd. <a href="../demo/vl-action-group.html">Demo</a>.
+ *
+ * @extends NativeVlElement
+ *
+ * @property {string} align - Attribuut wordt gebruikt om ervoor te zorgen dat de onderliggende elementen worden gealigneerd. Mogelijkheden: align="center" of align="right".
+ * @property {boolean} space-between - Attribuut wordt gebruikt om aan te duiden dat de ruimte tussen de elementen volledig moet worden opgevuld.
+ * @property {boolean} bordered - Attribuut wordt gebruikt om aan te duiden dat de tussenliggende elementen een rand krijgen.
+ */
+export class VlActionGroup extends NativeVlElement(HTMLDivElement) {
+
+  connectedCallback() {
+    this.classList.add('vl-action-group');
+  }
+
+  get _classPrefix() {
+    return 'vl-action-group--';
+  }
+
+  get _stylePath() {
+    return '../style.css';
+  }
+
+
+  static get _observedClassAttributes() {
+    return ['align', 'space-between', 'bordered'];
+  }
+
+  _alignChangedCallback(oldValue, newValue){
+    this._changeClass(this, ('align-' + oldValue), ('align-' + newValue), this._classPrefix);
+  }
+
+}
+
+customElements.define('vl-action-group', VlActionGroup,{extends: 'div'});

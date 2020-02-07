0b99ddff610a662478a7c8b9d8a9f91e62f39ed0
cambiph <incubusattax@gmail.com>
2020-02-07 11:45:12 +0100

    Eerste versie van script om style.css te vervangen

---
+++ scripts/copy-style.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+
+copyCssAndRenameCss() {
+    for dir in $1; do
+        pushd $PWD/node_modules/$dir \
+        && if [ -f "style.css" ]; then cp style.css ../../$dir.css; fi \
+        && popd
+    done
+}
+
+DIRS=$(ls "node_modules/" | grep -q "vl-ui-*")
+copyCssAndRenameCss $DIRS
+
+CSSFILES=$(ls | grep -E "vl-ui-(\w)*(.)css")
+
+HTMLFILES=()
+
+for file in $CSSFILES; do
+    STRIPPED=${file::-4}.html
+    CLEANED="${STRIPPED//-ui}"
+    HTMLFILES+=($CLEANED)
+done
+
+SEARCHSTRING="<link rel=\"stylesheet\" type=\"text/css\" href=\"/style.css\"/>"
+
+for file in ${HTMLFILES[@]}; do
+    # check if file exists (demo/vl-component.html)
+    if [ -f "demo/$file" ]; then 
+        # check if searchstring exists in file (demo/vl-component.html)
+        if grep -q "$SEARCHSTRING" "demo/$file"; then 
+            # change searchstring with new string which refers to correct css
+            RESULTSTRING="<link rel=\"stylesheet\" type=\"text/css\" href=\"/${file::-4}css\"/>"
+            sed -i "" -e "s%${SEARCHSTRING}%${RESULTSTRING}%" "demo/"$file
+            echo "Replaced path to style for file $file"
+        fi
+    fi
+done

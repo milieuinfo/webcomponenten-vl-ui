a0f2be16158f24783268ee70048278be8ec0faf2
Tom Coemans <tom.coemans@gmail.com>
2020-06-12 13:58:03 +0200

    update .mocharc.js to run all tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-util/test/**/*.test.js"],
+    "spec": ["node_modules/vl-ui-*/test/**/*.test.js"],
     "timeout": '10000'
 };

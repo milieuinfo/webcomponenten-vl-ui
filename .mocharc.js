e4621017b610d457aedf14613a9b8e08e01c4422
Tom Coemans <tom.coemans@gmail.com>
2020-07-16 12:21:13 +0200

    exclude cookie-consent tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/**/!(accordion|core).test.js"],
+    "spec": ["node_modules/vl-ui-*/test/**/!(accordion|core|cookie-consent).test.js"],
     "timeout": '10000'
 };

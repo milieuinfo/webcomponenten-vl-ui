eea21e56d7fa71d40371aaf2f7f106b5193a4022
Tom Coemans <tom.coemans@gmail.com>
2020-07-16 12:12:17 +0200

    exclude core tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/**/!(accordion).test.js"],
+    "spec": ["node_modules/vl-ui-*/test/**/!(accordion|core).test.js"],
     "timeout": '10000'
 };

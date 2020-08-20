3c5188ad457fd1aa46f77aa6cbb9a0db08fcc127
Tom Coemans <tom.coemans@gmail.com>
2020-08-20 11:05:38 +0200

    enable accordion e2e tests

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/e2e/!(accordion).test.js"],
+    "spec": ["node_modules/vl-ui-*/test/**/*.test.js"],
     "timeout": '10000'
 };

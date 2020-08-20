cc80cb914b55b9a549207cd5231cf2b164e1fed6
Tom Coemans <tom.coemans@gmail.com>
2020-08-20 13:41:16 +0200

    update dependencies

--- .mocharc.js
+++ .mocharc.js
@@ -1,6 +1,6 @@
 module.exports = {
     "full-trace": true,
     "reporter": 'spec',
-    "spec": ["node_modules/vl-ui-*/test/**/*.test.js"],
+    "spec": ["node_modules/vl-ui-*/test/e2e/!(select-action|layer|circle*).test.js"],
     "timeout": '10000'
 };
